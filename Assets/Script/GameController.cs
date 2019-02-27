d49f30f17a56e68fc22306334dac6895646cabbd
dashjay <dashjay@foxmail.com>
2019-02-27 13:50:09 +0800

    init

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    private string version = "Version: 0.0.2";
+    private GameObject _version;
+
+    public GameObject Male;
+    public GameObject Female;
+
+    private void Start()
+    {
+        _version = GameObject.Find("Version");
+        _version.GetComponent<Text>().text = version;
+    }
+
+    public void ChangeModelClick()
+    {
+        Male.SetActive(!Male.activeSelf);
+        Female.SetActive(!Female.activeSelf);
+    }
+}
---
+++ Assets/Script/UiController.cs
@@ -0,0 +1,59 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+    public Camera main;
+
+    public Vector3 originPosition;
+
+    public GameObject CastCamTarget;
+
+    public Button StartButton;
+
+    public Button CastButton;
+
+    public Button BackButton;
+
+    public Text Cast;
+
+    public float speed;
+
+    public float castSpeed;
+
+
+    private void Start()
+    {
+        //获取初始位置 
+        originPosition = main.transform.position;
+
+
+        Cast.gameObject.SetActive(false);
+        BackButton.gameObject.SetActive(false);
+    }
+
+    public void CastClick()
+    {
+        main.transform.DOMove(CastCamTarget.transform.position, speed * Time.deltaTime).SetEase(Ease.InOutSine);
+
+        StartButton.gameObject.SetActive(false);
+        CastButton.gameObject.SetActive(false);
+        Cast.gameObject.SetActive(true);
+
+        Cast.transform.DOLocalMoveY(986, castSpeed * Time.deltaTime).SetEase(Ease.Linear);
+
+        BackButton.gameObject.SetActive(true);
+    }
+
+    public void BackInit()
+    {
+        main.transform.DOMove(originPosition, speed * Time.deltaTime).SetEase(Ease.InOutSine);
+
+        StartButton.gameObject.SetActive(true);
+        CastButton.gameObject.SetActive(true);
+        Cast.gameObject.SetActive(false);
+
+        BackButton.gameObject.SetActive(false);
+    }
+}
